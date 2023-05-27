3e3de41d74410680e0f120c20a79142e7d43937f
MIYAKA <75727824+BambinoRiya@users.noreply.github.com>
2023-05-27 10:52:51 +0000

    Create percentageLetter.py

---
+++ percentageLetter.py
@@ -0,0 +1,13 @@
+class Solution:
+    def percentageLetter(self, s: str, letter: str) -> int:
+        newString = ""
+
+        for letter in s:
+            len(s) == newString
+
+        return newString
+
+        sumNum = (newString/len(s))
+        percentage = sumNum * 100
+
+        return percentage
