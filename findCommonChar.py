15bddaabd01e638c71348bf26f9cd5045db9af9d
MIYAKA <75727824+BambinoRiya@users.noreply.github.com>
2023-06-16 13:54:30 +0000

    Create findCommonChar.py

---
+++ findCommonChar.py
@@ -0,0 +1,11 @@
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        check = list(words[0])
+        for word in words[1:]:
+            new_check = []
+            for i in word:
+                if i in check:
+                    new_check.append(i)
+                    check.remove(i)
+            check = new_check
+        return check
