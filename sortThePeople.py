3fc71089f1f4c7eb0c3728c1f02916bb77ec9a3a
MIYAKA <75727824+BambinoRiya@users.noreply.github.com>
2023-06-30 16:55:29 +0000

    Update sortThePeople.py

--- sortThePeople.py
+++ sortThePeople.py
@@ -3,9 +3,11 @@ class Solution:
         for i in range(len(heights)):
             for j in range(len(heights)-1):
                 if heights[j] < heights[j+1]:
+                    heights[j],heights[j+1] = heights[j+1],heights[j]
                     names[j],names[j+1] = names[j+1],names[j]
+
 
         return names
 
-        #time complexity O(n)
+        #time complexity O(n**2)
         #space complexity O(1)
