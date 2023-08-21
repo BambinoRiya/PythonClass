809cc3c04f49744a11bbac0ed17427401546c8d9
MIYAKA <75727824+BambinoRiya@users.noreply.github.com>
2023-08-21 22:26:23 +0000

    Create EhabIsOddPerson.py

---
+++ EhabIsOddPerson.py
@@ -0,0 +1,10 @@
+odd_nums, even_nums = 0, 0
+for i in range(n):
+    if a[i] % 2 ==0:
+        even_nums += 1
+    else:
+        odd_nums += 1
+        
+if even_nums > 0 and odd_nums > 0:
+    a.sort()
+print(*a)
