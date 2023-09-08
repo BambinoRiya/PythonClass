9cadf9fa1c92a3646d5231a06545c0df68ccc6f8
MIYAKA <75727824+BambinoRiya@users.noreply.github.com>
2023-09-08 10:13:51 +0000

    Create deleteDuplicates.py

---
+++ campProblems/deleteDuplicates.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        temp = head
+        while temp and temp.next:
+            if temp.next.val == temp.val:
+                temp.next = temp.next.next
+                continue
+            temp = temp.next
+        return head
+
+#Space complexity : O(1)
+#time complexity : O(n)
+        
