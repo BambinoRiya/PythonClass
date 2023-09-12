22850a0067085d62c1cf0953296013242b08afb0
MIYAKA <75727824+BambinoRiya@users.noreply.github.com>
2023-09-12 09:50:34 +0000

    Update mergeSortedList.py

--- campProblems/mergeSortedList.py
+++ campProblems/mergeSortedList.py
@@ -5,20 +5,20 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode()
-        current = dummy
-
-        while list1 and list2:
+        dummy = temp = ListNode(0)
+        while list1 is not None and list2 is not None:
             if list1.val < list2.val:
-                current.next = list1
+                temp.next = list1
                 list1 = list1.next
             else:
-                current.next = list2
+                temp.next = list2
                 list2 = list2.next
-            current = current.next
-        if list1:
-            current.next = list1
-        if list2:
-            current.next = list2
-
+            temp = temp.next
+        temp.next = list1 or list2
         return dummy.next
+
+
+        #Time complexity : O(m+n)
+        #Space Complexity O(1)
+
+    
