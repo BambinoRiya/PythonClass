7b7007c824464066544c10dff300e34050377f66
MIYAKA <75727824+BambinoRiya@users.noreply.github.com>
2023-09-19 09:55:59 +0000

    Update kokoBananas.py

--- campProblems/kokoBananas.py
+++ campProblems/kokoBananas.py
@@ -1,14 +1,12 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         left,right= 1,max(piles)
-        while left<=right:
+        while left< right:
             mid = left + (right-left)//2
             total = sum([math.ceil(pile/mid) for pile in piles])
             if h < total:
                 left = mid + 1
-            elif h > total:
-                right = mid - 1
             else:
-                return mid
+                right = mid
         return left
         
