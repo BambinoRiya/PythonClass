17557abede31d72de63248657e2fda5290ae79f8
MIYAKA <75727824+BambinoRiya@users.noreply.github.com>
2023-11-08 16:04:10 +0000

    Update climbStairs.py

--- climbStairs.py
+++ climbStairs.py
@@ -1,9 +1,11 @@
 class Solution:
     @cache
     def climbStairs(self, n: int) -> int:
-        #base case
         if n == 0:
-            return 1
-        if n < 0:
-            return 0
+            return n
+        elif n == 1:
+            return n
+        elif n == 2:
+            return n
         return self.climbStairs(n-2) + self.climbStairs(n-1)
+        
