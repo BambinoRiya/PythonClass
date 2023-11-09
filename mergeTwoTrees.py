62248a12b19580552500a8ff9b962ddb5d797aed
MIYAKA <75727824+BambinoRiya@users.noreply.github.com>
2023-11-09 14:47:46 +0000

    Create mergeTwoTrees.py

---
+++ mergeTwoTrees.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        root3 = TreeNode()
+        if not root1 and not root2:
+            return None
+
+        if root1 and root2:
+            root3.val= root1.val + root2.val
+            root3.left = self.mergeTrees(root1.left,root2.left)
+            root3.right = self.mergeTrees(root1.right, root2.right)
+
+        if root1 and not root2:
+            root3 = root1
+            
+        if root2 and not root1:
+            root3 = root2
+
+        return root3
