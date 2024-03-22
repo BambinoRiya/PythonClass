fb66e37e2ffa6990b6a95f6b247db14f4038c516
MIYAKA <75727824+BambinoRiya@users.noreply.github.com>
2024-03-22 14:57:03 +0000

    Create haystackInNeedle.py

---
+++ haystackInNeedle.py
@@ -0,0 +1,16 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        m = len(needle)
+
+        if m > len(haystack):
+            return -1
+
+        for i in range(len(haystack)):
+            if needle == haystack[i:i+m]:
+                return i
+                continue
+            
+        return -1
+
+
+        
