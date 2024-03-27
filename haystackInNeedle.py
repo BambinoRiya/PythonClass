b20b8cd9f445701e35134518184a6fcfb6257cde
MIYAKA <75727824+BambinoRiya@users.noreply.github.com>
2024-03-27 11:57:47 +0000

    Update haystackInNeedle.py

--- haystackInNeedle.py
+++ haystackInNeedle.py
@@ -12,5 +12,38 @@ class Solution:
             
         return -1
 
-#Robin Karp algorithm to be done by Sunday
+#Robin Karp algorithm 
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        def transform(word):
+            n = len(word)
+            curr = 0
+            for i in range(len(word)):
+                curr += ((ord(word[i]) - ord('a')+ 1) * (26**(n-i)))
+
+            return curr
+
+        ans = transform(needle)
+
+        left = 0
+        right = len(needle) - 1
+
+        pre = transform(haystack[0:len(needle)])
+
+        while right < (len(haystack))-1:
+            if pre == ans:
+                return left
+            else:
+                left +=1
+                right+=1
+                pre = (26*(pre - ((ord(haystack[left-1]) - ord('a') + 1) * (26**(len(needle)-1))))) + (ord(haystack[right]) - ord('a') + 1)
+                #precompute
+
+        if pre == ans:
+            return left
+        else:
+        
+            return -1
+        
         
